4cd6161f772d480065e41960fadeb45742809c43
Lexxkit <lexxkit@gmail.com>
2022-04-26 11:35:42 +0300

    Override equals and hashCode methods in Book and Author classes.

--- src/com/lexxkit/Main.java
+++ src/com/lexxkit/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
         Author rowling = new Author("Joanne", "K.Rowling");
         Author herbert = new Author("Frank", "Herbert");
+        Author rowling2 = new Author("Joanne", "Rowling");
 
         Book harryPotter = new Book("Harry Potter and the Philosopher's Stone", rowling, 1996);
         Book dune = new Book("Dune", herbert, 1965);
+        Book harryPotter2 = new Book("Harry Poter Pt.2", rowling, 1997);
 
         System.out.println(harryPotter);
 
@@ -17,5 +19,17 @@ public class Main {
         System.out.println(harryPotter);
 
         System.out.println(dune);
+
+        if (harryPotter.equals(harryPotter2)) {
+            System.out.println("Books are equal.");
+        } else {
+            System.out.println("Books are different.");
+        }
+
+        if (rowling.equals(rowling2)) {
+            System.out.println("This is the same author.");
+        } else {
+            System.out.println("Authors are different.");
+        }
     }
 }
